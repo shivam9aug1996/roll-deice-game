47f17f138f6b2450634082d431285c5cdaad316a
Shivam Garg <shivam@Shivams-MacBook-Air.local>
2022-02-25 11:44:42 +0530

    optimized using lazy and suspense

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,11 +2,11 @@ import React from "react";
 import loader from "../images/loader.gif";
 const Loader = () => {
   return (
-    <>
+    <div className="container container-margin">
       <div className="center-loader">
         <img src={loader} />
       </div>
-    </>
+    </div>
   );
 };
 
