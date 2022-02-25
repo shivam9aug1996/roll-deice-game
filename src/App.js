45a0e0dfccd9263984aac227b641306f8a51f066
Shivam Garg <shivam@Shivams-MacBook-Air.local>
2022-02-25 11:21:54 +0530

    optimized using lazy and suspense

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
-const Footer = lazy(() => import("./components/Footer"));
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+//const Footer = lazy(() => import("./components/Footer"));
 const Main = lazy(() => import("./components/Main"));
-const Navbar = lazy(() => import("./components/Navbar"));
+//const Navbar = lazy(() => import("./components/Navbar"));
 const About = lazy(() => import("./components/About"));
 const Shop = lazy(() => import("./components/Shop"));
 const Product = lazy(() => import("./components/Product"));
@@ -13,23 +15,21 @@ const ScrollToTop = lazy(() => import("./components/ScrollToTop"));
 const App = () => {
   return (
     <>
-      <div>
-        <Suspense fallback={<div></div>}>
-          <>
-            <Navbar />
-            <ScrollToTop />
-            <Routes>
-              <Route path="/" element={<Main />} />
-              <Route path="about" element={<About />} />
-              <Route path="shop" element={<Shop />} />
-              <Route path="shop/:id" element={<Product />} />
-              <Route path="cart" element={<Cart />} />
-              <Route path="order" element={<OrderHistory />} />
-            </Routes>
-            <Footer />
-          </>
-        </Suspense>
-      </div>
+      <Navbar />
+      <Suspense fallback={<div></div>}>
+        <>
+          <ScrollToTop />
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="about" element={<About />} />
+            <Route path="shop" element={<Shop />} />
+            <Route path="shop/:id" element={<Product />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="order" element={<OrderHistory />} />
+          </Routes>
+        </>
+      </Suspense>
+      <Footer />
     </>
   );
 };
