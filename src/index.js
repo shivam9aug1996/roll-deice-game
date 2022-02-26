72f6fefb2975dd2b0058d0494710a6f6132b4a7e
Shivam Garg <shivam@Shivams-MacBook-Air.local>
2022-02-26 15:38:27 +0530

    added cache control in _redircet

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import $ from "jquery";
 import Popper from "popper.js";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import { BrowserRouter } from "react-router-dom";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 const App = lazy(() => import("./App"));
 ReactDOM.render(
   <React.StrictMode>
@@ -23,11 +22,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
